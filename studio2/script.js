cfac233f4afa6c59b50ce1bee90522a9ece797bd
Ash Chang <asichang@ucdavis.edu>
2023-11-07 16:50:18 -0800

    journal 5

--- studio2/script.js
+++ studio2/script.js
@@ -1,4 +1,45 @@
 (function(){
     'use strict'
     
+    const moon = document.querySelector('#moon');
+    const purple = document.querySelector('#purple');
+    const cow = document.querySelector('#cow');
+    const leopard = document.querySelector('#leopard');
+    const nettle = document.querySelector('#nettle');
+
+    moon.addEventListener('click', function(){
+        document.querySelector('#moonjelly').className = 'showing';
+        document.querySelector('button').addEventListener('click', function(){
+            document.querySelector('#moonjelly').className = 'hidden';
+        })
+    })
+    
+    purple.addEventListener('click', function(){
+        document.querySelector('#purplestrippedjelly').className = 'showing';
+        document.querySelector('button').addEventListener('click', function(){
+            document.querySelector('#purplestrippedjelly').className = 'hidden';
+        })
+    })
+
+    cow.addEventListener('click', function(){
+        document.querySelector('#cownoseray').className = 'showing';
+        document.querySelector('button').addEventListener('click', function(){
+            document.querySelector('#cownoseray').className = 'hidden';
+        })
+    })
+
+    leopard.addEventListener('click', function(){
+        document.querySelector('#leopardsharks').className = 'showing';
+        document.querySelector('button').addEventListener('click', function(){
+            document.querySelector('#leopardsharks').className = 'hidden';
+        })
+    })
+
+    nettle.addEventListener('click', function(){
+        document.querySelector('#japaneseseanettle').className = 'showing';
+        document.querySelector('button').addEventListener('click', function(){
+            document.querySelector('#japaneseseanettle').className = 'hidden';
+        })
+    })
+
 })();
