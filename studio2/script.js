ae3df6b35fc9fa3ac78b6d216029d0433d3ea247
Ash Chang <asichang@ucdavis.edu>
2023-11-11 17:15:37 -0800

    studio 2

--- studio2/script.js
+++ studio2/script.js
@@ -1,45 +1,54 @@
 (function(){
     'use strict'
-    
-    const moon = document.querySelector('#moon');
-    const purple = document.querySelector('#purple');
-    const cow = document.querySelector('#cow');
-    const leopard = document.querySelector('#leopard');
-    const nettle = document.querySelector('#nettle');
 
-    moon.addEventListener('click', function(){
-        document.querySelector('#moonjelly').className = 'showing';
-        document.querySelector('button').addEventListener('click', function(){
-            document.querySelector('#moonjelly').className = 'hidden';
-        })
-    })
-    
-    purple.addEventListener('click', function(){
-        document.querySelector('#purplestrippedjelly').className = 'showing';
-        document.querySelector('button').addEventListener('click', function(){
-            document.querySelector('#purplestrippedjelly').className = 'hidden';
-        })
-    })
 
-    cow.addEventListener('click', function(){
-        document.querySelector('#cownoseray').className = 'showing';
-        document.querySelector('button').addEventListener('click', function(){
-            document.querySelector('#cownoseray').className = 'hidden';
-        })
-    })
+    const jellies = [
+        document.querySelector('#moon'),
+        document.querySelector('#purple'),
+        document.querySelector('#cow'),
+        document.querySelector('#leopard'),
+        document.querySelector('#nettle')
+    ];
 
-    leopard.addEventListener('click', function(){
-        document.querySelector('#leopardsharks').className = 'showing';
-        document.querySelector('button').addEventListener('click', function(){
-            document.querySelector('#leopardsharks').className = 'hidden';
-        })
-    })
+    const info = [
+        document.querySelector('#moonjelly'),
+        document.querySelector('#purplestrippedjelly'),
+        document.querySelector('#cownoseray'),
+        document.querySelector('#leopardsharks'),
+        document.querySelector('#japaneseseanettle')
+    ];
 
-    nettle.addEventListener('click', function(){
-        document.querySelector('#japaneseseanettle').className = 'showing';
-        document.querySelector('button').addEventListener('click', function(){
-            document.querySelector('#japaneseseanettle').className = 'hidden';
-        })
-    })
+    const button = document.querySelectorAll('button');
+    const body = document.querySelector('body');
+
+
+    body.style.overflow = 'auto';
+
+    for (let i=0; i<jellies.length; i++){
+        jellies[i].addEventListener('click', function(){
+            jellies[i].className = 'zoomIn';
+            info[i].className = 'showing';
+            const imageContainerRect = jellies[i].getBoundingClientRect();
+            window.scrollTo({
+              left: window.scrollX + imageContainerRect.left-800,
+              behavior: 'smooth'
+            });
+            body.style.overflow = 'hidden';
+            
+        });
+
+        button[i].addEventListener('click', function(){
+            info[i].className = 'hidden';
+            jellies[i].className = '';
+            body.style.overflow = 'auto';
+        });
+
+        document.addEventListener('click', function(e){
+            if(!jellies[i].contains(e.target)){
+                info[i].className = 'hidden';
+                jellies[i].className = '';
+            }
+        });
+    };
 
 })();
