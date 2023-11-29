4967131189b874a09d7508ed7b478d2b4823daf0
Ash Chang <asichang@ucdavis.edu>
2023-11-29 00:27:35 -0800

    game

--- studio3/script.js
+++ studio3/script.js
@@ -28,11 +28,14 @@
         } else {
             document.querySelector('#intro').className = 'hidden';
             document.querySelector('#play').className = 'showing';
-            explain.innerHTML = `<p>In the big city of New York, <strong>${rat1}</strong> and <strong>${rat2}</strong> are rats scrambling to reach the largest slice of pizza on the subway floor. The goal of the game is to get to the pizza first by advancing your steps based on the total number each rat rolls. However, beware! The turn is ended if a one comes up on either dice. If both dice are ones, you have cat eyes, and your score returns to zero.</p>`;
+            explain.innerHTML = `<p>In the big city of New York, <strong>${rat1}</strong> and <strong>${rat2}</strong> are rats scrambling to reach the largest slice of pizza on the subway floor.</p> <p>The goal of the game? Be the first rat to get to the pizza by advancing your steps based on the total number each rat rolls.</p> 
+            <p>However, beware of the cat lurking around! If a one comes up on either pizza, the turn is ended. If both pizza are ones, you've been caught by the cat, and your score returns to zero.</p>`;
 
             startPlay.addEventListener('click', function(){
+                playTap();
                 document.querySelector('#rules').className = 'hidden';
                 document.querySelector('#start').className = 'showing';
+                start
                 setUpTurn();            
             });
 
@@ -52,57 +55,29 @@
         gameEnd: 29
     };
 
-    const startGame = document.querySelector('#startgame');
+    const startGame = document.querySelector('#startPlay');
     const gameControl = document.querySelector('#gamecontrol');
     const game = document.querySelector('#game');
     const score = document.querySelector('#score');
     const actionArea = document.querySelector('#actions');
 
-
-    //This gets the current player: 
-    // gameData.players[gameData.index]
-
-    // //This gets the first die and the second die: 
-    // gameData.dice[gameData.roll1-1]
-    // gameData.dice[gameData.roll2-1]
-
-    // //This gets the score of the current player: 
-    // gameData.score[gameData.index]
-
-    // //This gets the index, or turn
-    // gameData.index
-
-    // //This gets the individual dice values and the added dice value
-    // gameData.roll1
-    // gameData.roll2
-    // gameData.rollSum
-
-    // //This gets the winning threshold
-    // gameData.rollSum
-
-
     startGame.addEventListener('click', function(){
-        //random set of game index
         gameData.index = Math.round(Math.random());
         console.log(gameData.index);
-
-        gameControl.innerHTML = '<h2>the game has started</h2>';
-
-        // gameControl.innerHTML += '<button id="quit">wanna quit?</button>';
-        // document.getElementById('quit').addEventListener('click', function(){
-        //     location.reload();
-        // });
         setUpTurn();            
-
     });
 
-
-
     function setUpTurn(){
-        game.innerHTML = `<p>Roll the dice for ${ gameData.players[gameData.index] }</p>`;
-        actionArea.innerHTML = '<button id="roll">roll the dice</button>';
+        game.innerHTML = `<p>Roll the pizza for ${ gameData.players[gameData.index] }</p>`;
+        actionArea.innerHTML = '<button id="roll">roll the pizza</button>';
+        document.querySelector('#score').className = 'hidden';
+        document.querySelector('#brat').innerHTML = `<h2>${rat1}</h2> <img src="images/brat.png" alt= "brown rat">`;
+        document.querySelector('#grat').innerHTML = `<h2>${rat2}</h2> <img src="images/grat.png" alt= "gray rat">`;
+
         document.getElementById('roll').addEventListener('click', function(){
             throwDice();
+            playRun();
+            document.querySelector('#score').className = 'showing';
         });
     };
 
@@ -110,13 +85,13 @@
         actionArea.innerHTML = '';
         gameData.roll1 = Math.floor(Math.random()*6)+1;
         gameData.roll2 = Math.floor(Math.random()*6)+1;
-        game.innerHTML=`<p>roll the dice for ${gameData.players[gameData.index]}</p>`;
-        game.innerHTML += `<img src = "${gameData.dice[gameData.roll1-1]}">  <img src = "${gameData.dice[gameData.roll2-1]}">`;
+        game.innerHTML=`<p>Roll the pizza for ${gameData.players[gameData.index]}</p>`;
+        game.innerHTML += `<article id="pizzaDice"><img src = "${gameData.dice[gameData.roll1-1]}">  <img src = "${gameData.dice[gameData.roll2-1]}"></article>`;
         gameData.rollSum = gameData.roll1 + gameData.roll2;
         //console.log(gameData.rollSum);
 
         if (gameData.rollSum === 2){
-            game.innerHTML += '<p>oh snap, cat eyes</p>';
+            game.innerHTML += '<p>You have been caught by the cat!</p>';
             gameData.score [gameData.index] = 0;
             gameData.index ? (gameData.index =0) : (gameData.index =1);
             setTimeout(setUpTurn, 2000);
@@ -132,7 +107,7 @@
             actionArea.innerHTML = '<button id="rollagain">roll again</button> or <button id="pass">pass</button>';
 
             document.getElementById('rollagain').addEventListener('click', function(){
-                setUpTurn();
+                throwDice();
             });
 
             document.getElementById('pass').addEventListener('click', function(){
@@ -143,19 +118,38 @@
         checkWinningCondition();
     };   
 
-    // function checkWinningCondition(){
-    //     if (gameData.score[gameData.index] > gameData.gameEnd){
-    //         score.innerHTML = `<h2>${gameData.players[gameData.index]} wins the ${gameData.score[gameData.index]} points</h2>`;
+    function checkWinningCondition(){
+        if (gameData.score[gameData.index] > gameData.gameEnd){
+            score.innerHTML = `<h2>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points</h2>`;
 
-    //         actionArea.innerHTML='';
-    //         document.getElementById('quit').innerHTML = "Start a new game?";
-    //     } else{
-    //         score.innerHTML = `<p>the score for ${gameData.players[0]} is <strong> ${gameData.score[0]} </strong> and the score for ${gameData.players[1]} is <strong> ${gameData.score[1]} </strong> </p>`;
-    //         showCurrentScore();
-    //     }
-    // }
+            actionArea.innerHTML='';
+            document.getElementById('quit').innerHTML += '<button id="quit">start a new game</button>';
+            document.getElementById('quit').addEventListener('click', function(){
+                location.reload();
+            });
+        } else{
+            score.innerHTML = `<p>the score for ${gameData.players[0]} is <strong> ${gameData.score[0]} </strong> and the score for ${gameData.players[1]} is <strong> ${gameData.score[1]} </strong> </p>`;
+            showCurrentScore();
+        }
+    }
 
-    // function showCurrentScore(){
-    //     score.innerHTML = `<p>the score for ${gameData.players[0]} is <strong> ${gameData.score[0]} </strong> and the score for ${gameData.players[1]} is <strong> ${gameData.score[1]} </strong> </p>`;
-    // }
+    function showCurrentScore(){
+        score.innerHTML = `<p>the score for ${gameData.players[0]} is <strong> ${gameData.score[0]} </strong> and the score for ${gameData.players[1]} is <strong> ${gameData.score[1]} </strong> </p>`;
+    }
+
+
+    function playTap() {
+        const audio = new Audio('sounds/tap.m4a'); 
+        audio.play();
+    }
+    function playRun() {
+        const audio = new Audio('sounds/run.m4a'); 
+        audio.play();
+    }
+    
+    document.getElementById('readRules').addEventListener('click', function() {
+        playTap();
+    });
+
+
 })(); 
